bb5d324d63d79f7244da3f8899ef67919e61ab27
Emmanuel <zhang1357998@gmail.com>
2020-11-10 06:22:08 +0000

    Add files via upload

--- src/university/doctoralStudent.java
+++ src/university/doctoralStudent.java
@@ -1,9 +1,14 @@
 package university;
 
-public class doctoralStudent extends salary implements studentManage,teacherManage {
+public class doctoralStudent implements studentManage,teacherManage {
     private String name;
     private String gender;
     private int age;
+    private double fee;
+    private double salary;
+    private double yFee;
+    private double ySalary;
+    private double tax;
     public doctoralStudent(String name,String gender,int age){
         //new student....
         super();
@@ -27,27 +32,82 @@ public class doctoralStudent extends salary implements studentManage,teacherMana
         return age;
     }
 
-    @Override
-    public double getStudentFee() {
-        //no use for because i put tax out
-        return 0;
+    /**
+     * this is what tax i use
+     * salary tax% number
+     * <5000   0%     0
+     * <8000   3%     0
+     * <17000  10%    210
+     * <30000  20%    1440
+     * <40000  25%    2660
+     * <60000  30%    4410
+     * <85000  35%    7160
+     * >85000  40%    15160
+     * */
+
+    void setSalary(double salary){
+        //set salary
+        this.salary = salary;
+    }
+
+    void setFee(double fee){
+        //set Fee
+        this.fee = fee;
+    }
+    double getSalary(){
+        //once year salary
+        ySalary = salary * 12;
+        return ySalary;
+    }
+
+    double getFee(){
+        //once year Fee, because fee one year just twice so fee*2
+        yFee = fee * 2;
+        return yFee;
+    }
+
+    double getTax(){
+        //should tax how much money
+        double ntm;
+        ntm =salary - yFee/12;
+        if (salary <= 5000){
+            tax = ntm;
+        }else if(salary>5000&&salary<=8000){
+            tax = (((ntm - 5000) * 0.03)-0);
+        }else if(salary>8000&&salary<=17000){
+            tax = (((ntm - 5000) * 0.10)-210);
+        }else if(salary>17000&&salary<=30000){
+            tax = (((ntm - 5000) * 0.20)-1410);
+        }else if(salary>30000&&salary<=40000){
+            tax = (((ntm - 5000) * 0.25)-2660);
+        }else if(salary>40000&&salary<=60000){
+            tax = (((ntm - 5000) * 0.30)-4410);
+        }else if(salary>60000&&salary<=85000){
+            tax = (((ntm - 5000) * 0.35)-7160);
+        }else{
+            tax = (((ntm - 5000) * 0.40)-15160);
+        }
+        return tax*12;
     }
 
     @Override
+    public double getStudentFee() {
+        return yFee;
+    }
+
+
+    @Override
     public double checkStudentFee() {
-        //no use for because i put tax out
-        return 0;
+        return yFee;
     }
 
     @Override
     public double getTeacherSalary() {
-        //no use for because i put tax out
-        return 0;
+        return ySalary - getTax();
     }
 
     @Override
     public double checkTeacherSalary() {
-        //no use for because i put tax out
-        return 0;
+        return ySalary - getTax();
     }
 }
--- src/university/test.java
+++ src/university/test.java
@@ -10,20 +10,29 @@ public class test {
         try{
             //Scanner input
             Scanner a = new Scanner(System.in);
-            System.out.println("请输入第一位博士研究生的工资");
+            System.out.println("请输入第一位博士研究生的工资:");
             ds1.setSalary(a.nextDouble());
-            System.out.println("请输入第一位博士研究生的学费");
+            System.out.println("请输入第一位博士研究生的学费:");
             ds1.setFee(a.nextDouble());
-            System.out.println("请输入第二位博士研究生的工资");
+            System.out.println("请输入第二位博士研究生的工资:");
             ds2.setSalary(a.nextDouble());
-            System.out.println("请输入第二位博士研究生的学费");
+            System.out.println("请输入第二位博士研究生的学费:");
             ds2.setFee(a.nextDouble());
         }catch (NumberFormatException a){
             //Exception use
             System.out.println("math only");
+        }catch (Exception e){
+            //Exception use
+            System.out.println("math only");
         }
 
-        System.out.println("名字:" + ds1.getName() + " 性别:"+ ds1.getGender() + " 年龄:" + ds1.getAge() + " 工资:"+ ds1.getSalary() + " 学费:" + ds1.getFee() + " 税:" + ds1.getTax());
-        System.out.println("名字:" + ds2.getName() + " 性别:"+ ds2.getGender() + " 年龄:" + ds2.getAge() + " 工资:"+ ds2.getSalary() + " 学费:" + ds2.getFee() + " 税:" + ds2.getTax());
+        System.out.println(" 名字:" + ds1.getName() + " 性别:"+ ds1.getGender() + " 年龄:" + ds1.getAge() + " 工资:"+ ds1.getSalary() + " 学费:" + ds1.getFee() + " 税:" + ds1.getTax());
+        System.out.println(" 名字:" + ds2.getName() + " 性别:"+ ds2.getGender() + " 年龄:" + ds2.getAge() + " 工资:"+ ds2.getSalary() + " 学费:" + ds2.getFee() + " 税:" + ds2.getTax());
+        System.out.println(" 名字:" + ds1.getName() + " 学费:" + ds1.checkStudentFee());
+        System.out.println(" 名字:" + ds2.getName() + " 学费:" + ds2.checkStudentFee());
+        System.out.println(" 名字:" + ds1.getName() + " 工资:"+ ds1.checkTeacherSalary());
+        System.out.println(" 名字:" + ds2.getName() + " 工资:"+ ds2.checkTeacherSalary());
+
+
     }
 }
